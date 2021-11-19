0db4c77ef69cdfeef22e424d420204d04f582a9f
sandhyaprabhala <gayatri.1@iitj.ac.in>
2021-11-19 12:56:05 +0530

    initial commit of test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,33 @@
+import os
+import 
+
+def test_digit_correct_0():
+    temp 
+    assert temp == 0
+def test_digit_correct_1()):
+    temp 
+    assert temp == 1
+def test_digit_correct_2():
+    temp 
+    assert temp == 2
+def test_digit_correct_3():
+    temp 
+    assert temp == 3
+def test_digit_correct_4():
+    temp 
+    assert temp == 4
+def test_digit_correct_5():
+    temp 
+    assert temp == 5
+def test_digit_correct_6():
+    temp 
+    assert temp == 6
+def test_digit_correct_7():
+    temp 
+    assert temp == 7
+def test_digit_correct_8():
+    temp 
+    assert temp == 8
+def test_digit_correct_9():
+    temp 
+    assert temp == 9
